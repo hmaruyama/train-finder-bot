e5bd8ad162cafe25979801e3f35c01b29839b02c
hmaruyama <maruyama@val.co.jp>
2017-09-26 11:28:55 +0900

    不要なコメントを削除しました。

--- app.js
+++ app.js
@@ -4,22 +4,18 @@ var builder = require('botbuilder');
 require('dotenv').config();
 var request = require('request');
 
-// Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
   console.log('%s listening to %s', server.name, server.url);
 });
 
-// Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
   appId: process.env.MICROSOFT_APP_ID,
   appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
-// Listen for messages from users
 server.post('/api/messages', connector.listen());
 
-// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
 
   if(session.message.attachments.length > 0) {
